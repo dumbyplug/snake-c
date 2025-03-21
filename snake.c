3e4c28763cce0f8d3bd3b3b3227be8f0a3eec577
borday <iwasdumbyforu@gmail.com>
2025-03-21 11:17:13 +0400

    Snake moves by itself now

--- snake.c
+++ snake.c
@@ -9,9 +9,27 @@
 	rect.x -= rect.w / 2;
 	rect.y -= rect.h / 2;
 }*/
-int move(SDL_Rect *rect, int size, int x, int y){
+int move(SDL_Rect *rect, int size, int speed, int snake_facing){
 	// This function move the snake to the desired location if it is possible
-	int i;
+	int i, x, y;
+
+	if(snake_facing == 0){
+		x = rect[size - 1].x + speed;
+		y = rect[size - 1].y;
+	}
+	else if(snake_facing == 1){
+		x = rect[size - 1].x;
+		y = rect[size - 1].y - speed;
+	}
+	else if(snake_facing == 2){
+		x = rect[size - 1].x - speed;
+		y = rect[size - 1].y;
+	}
+
+	else if(snake_facing == 3){
+		x = rect[size - 1].x;
+		y = rect[size - 1].y + speed;
+	}
 
 	// checking if the snake would leave the canvas, when the move played 
 	if((x < 0) || (y < 0) ||
@@ -35,32 +53,73 @@ int move(SDL_Rect *rect, int size, int x, int y){
 }
 
 void snake_init(SDL_Rect *rect, int size){
+	// placing its body parts next to each other
 	for(int i = 0; i < size; i++){
 		rect[i].x = 0 + 40 * i;  rect[i].y = 360;
 		rect[i].w = 40;          rect[i].h = 40;
 	}
 }
 
+void draw_snake(SDL_Renderer *renderer,SDL_Rect *rect, 
+		SDL_Rect *eyes, int size, int snake_facing){
+
+		int index;
+		for(index = 0; index < size; index++){
+			SDL_RenderFillRect(renderer, rect + index);
+		}
+
+		if(snake_facing == 0){
+			eyes[0].x = rect[size - 1].x + 30; 
+			eyes[0].y = rect[size - 1].y + 10; 
+			eyes[1].x = rect[size - 1].x + 30; 
+			eyes[1].y = rect[size - 1].y + 25; 
+		}
+		else if(snake_facing == 1){
+			eyes[0].x = rect[size - 1].x + 10; 
+			eyes[0].y = rect[size - 1].y + 5; 
+			eyes[1].x = rect[size - 1].x + 25; 
+			eyes[1].y = rect[size - 1].y + 5; 
+		}
+		else if(snake_facing == 2){
+			eyes[0].x = rect[size - 1].x + 5; 
+			eyes[0].y = rect[size - 1].y + 10; 
+			eyes[1].x = rect[size - 1].x + 5; 
+			eyes[1].y = rect[size - 1].y + 25; 
+		}
+		else if(snake_facing == 3){
+			eyes[0].x = rect[size - 1].x + 10; 
+			eyes[0].y = rect[size - 1].y + 30; 
+			eyes[1].x = rect[size - 1].x + 25; 
+			eyes[1].y = rect[size - 1].y + 30; 
+		}
+		SDL_SetRenderDrawColor(renderer, 233, 15, 23, 255);
+		SDL_RenderFillRect(renderer, &eyes[0]);
+		SDL_RenderFillRect(renderer, &eyes[1]);
+		
+}
+
 int main(void){
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
+	SDL_Event event;
+
+	int size = 8, speed = 40;
+	SDL_Rect rect[400];
+	SDL_Rect eyes[2];
+	int snake_facing = 0; // 0 - right, 1 - up, 2 - left, 3 - down 
 
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(WINDOW_WIDTH, 
 			WINDOW_HEIGHT, 0, &window, &renderer);
 
-	int size = 8;
-	SDL_Rect rect[400];
 	snake_init(rect, size);
 
-	int speed = 40;
+	eyes[0].w = 5; eyes[0].h = 5;
+	eyes[1].w = 5; eyes[1].h = 5;
 
-	
-	SDL_Event event;
-		
 
 	int run = 1;
-	int index;
+	int move_intensity = 0;
 	while(run){
 		while(SDL_PollEvent(&event)){
 			if(event.type == SDL_QUIT)
@@ -70,29 +129,37 @@ int main(void){
 			switch(event.key.keysym.sym){
 			
 			case SDLK_RIGHT:
-				move(rect, size, rect[size - 1].x + speed, rect[size - 1].y);
+				if(snake_facing != 2)
+					snake_facing = 0;
 				break;
 			case SDLK_LEFT:
-				move(rect, size, rect[size - 1].x - speed, rect[size - 1].y);
+				if(snake_facing != 0)
+					snake_facing = 2;
 				break;
 			case SDLK_UP:
-				move(rect, size, rect[size - 1].x, rect[size - 1].y - speed);
+				if(snake_facing != 3)
+					snake_facing = 1;
 				break;
 			case SDLK_DOWN:
-				move(rect, size, rect[size - 1].x, rect[size - 1].y + speed);
+				if(snake_facing != 1)
+					snake_facing = 3;
 				break;
-			}	
+			}
 			}
 		}
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_SetRenderDrawColor(renderer, 150, 213, 205, 255);
 		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		
-		for(index = 0; index < size; index++){
-			SDL_RenderFillRect(renderer, rect + index);
+
+		SDL_SetRenderDrawColor(renderer, 8, 15, 23, 255);
+		if(move_intensity > 18){
+			move(rect, size, speed, snake_facing);
+			move_intensity = 1;
 		}
+		draw_snake(renderer, rect, eyes, size, snake_facing);
+		
 		SDL_RenderPresent(renderer);
 		SDL_Delay(10);
+		move_intensity++;
 	}
 	
 	return 0;
